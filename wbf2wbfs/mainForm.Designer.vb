d9104a793457ba944c83dd45706459260b9c6ec2
sc-zhang <zsc-zhang@foxmail.com>
2023-04-21 00:02:55 +0800

    upload files

---
+++ wbf2wbfs/mainForm.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.lstvConv = New System.Windows.Forms.ListView()
+        Me.cohFilename = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cohStatus = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.cmdAdd = New System.Windows.Forms.Button()
+        Me.cmdDel = New System.Windows.Forms.Button()
+        Me.cmdClr = New System.Windows.Forms.Button()
+        Me.cmdConv = New System.Windows.Forms.Button()
+        Me.cdlOpen = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'lstvConv
+        '
+        Me.lstvConv.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.cohFilename, Me.cohStatus})
+        Me.lstvConv.FullRowSelect = True
+        Me.lstvConv.GridLines = True
+        Me.lstvConv.Location = New System.Drawing.Point(4, 3)
+        Me.lstvConv.Name = "lstvConv"
+        Me.lstvConv.Size = New System.Drawing.Size(332, 257)
+        Me.lstvConv.TabIndex = 0
+        Me.lstvConv.UseCompatibleStateImageBehavior = False
+        Me.lstvConv.View = System.Windows.Forms.View.Details
+        '
+        'cohFilename
+        '
+        Me.cohFilename.Text = "File"
+        Me.cohFilename.Width = 117
+        '
+        'cohStatus
+        '
+        Me.cohStatus.Text = "Status"
+        Me.cohStatus.Width = 203
+        '
+        'cmdAdd
+        '
+        Me.cmdAdd.Location = New System.Drawing.Point(4, 266)
+        Me.cmdAdd.Name = "cmdAdd"
+        Me.cmdAdd.Size = New System.Drawing.Size(53, 23)
+        Me.cmdAdd.TabIndex = 1
+        Me.cmdAdd.Text = "Add"
+        Me.cmdAdd.UseVisualStyleBackColor = True
+        '
+        'cmdDel
+        '
+        Me.cmdDel.Location = New System.Drawing.Point(63, 266)
+        Me.cmdDel.Name = "cmdDel"
+        Me.cmdDel.Size = New System.Drawing.Size(53, 23)
+        Me.cmdDel.TabIndex = 2
+        Me.cmdDel.Text = "Delete"
+        Me.cmdDel.UseVisualStyleBackColor = True
+        '
+        'cmdClr
+        '
+        Me.cmdClr.Location = New System.Drawing.Point(122, 266)
+        Me.cmdClr.Name = "cmdClr"
+        Me.cmdClr.Size = New System.Drawing.Size(53, 23)
+        Me.cmdClr.TabIndex = 3
+        Me.cmdClr.Text = "Clear"
+        Me.cmdClr.UseVisualStyleBackColor = True
+        '
+        'cmdConv
+        '
+        Me.cmdConv.Location = New System.Drawing.Point(180, 266)
+        Me.cmdConv.Name = "cmdConv"
+        Me.cmdConv.Size = New System.Drawing.Size(156, 23)
+        Me.cmdConv.TabIndex = 4
+        Me.cmdConv.Text = "*.wbf -> *.wbfs"
+        Me.cmdConv.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(341, 294)
+        Me.Controls.Add(Me.cmdConv)
+        Me.Controls.Add(Me.cmdClr)
+        Me.Controls.Add(Me.cmdDel)
+        Me.Controls.Add(Me.cmdAdd)
+        Me.Controls.Add(Me.lstvConv)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "wbf to wbfs"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstvConv As System.Windows.Forms.ListView
+    Friend WithEvents cmdAdd As System.Windows.Forms.Button
+    Friend WithEvents cmdDel As System.Windows.Forms.Button
+    Friend WithEvents cmdClr As System.Windows.Forms.Button
+    Friend WithEvents cmdConv As System.Windows.Forms.Button
+    Friend WithEvents cdlOpen As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents cohFilename As System.Windows.Forms.ColumnHeader
+    Friend WithEvents cohStatus As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ wbf2wbfs/mainForm.vb
@@ -0,0 +1,140 @@
+﻿Public Class frmMain
+
+    Private i As Integer
+    Private fill(498) As Byte
+
+    Private Sub wbf2wbfs(ByVal strfn As String, ByVal fn As Integer)
+        Dim filehead(0 To 3) As Byte
+        Dim fLen(0 To 3) As Byte
+        Dim dLen As Double
+        Dim FSO As Object
+
+        If Dir(strfn) = "" Then
+            lstvConv.Items(fn).SubItems(1).Text = "File not exists"
+            Exit Sub
+        End If
+
+        Try
+            FileSystem.FileOpen(1, strfn, OpenMode.Binary)
+            FileSystem.FileGet(1, filehead)
+            FileSystem.FileClose(1)
+
+            If filehead(0) <> 87 Or filehead(1) <> 66 Or filehead(2) <> 70 Or filehead(3) <> 83 Then
+                lstvConv.Items(fn).SubItems(1).Text = "Not WBFS file"
+                Exit Sub
+            End If
+
+            FSO = CreateObject("Scripting.FileSystemObject")
+            dLen = FSO.GetFile(strfn).Size
+            FSO = Nothing
+
+            dLen = dLen / 512
+            fLen(3) = dLen Mod 256
+            fLen(2) = (dLen \ 256) Mod 256
+            fLen(1) = (dLen \ 65536) Mod 256
+            fLen(0) = dLen \ 16777216
+
+            FileSystem.FileOpen(1, strfn, OpenMode.Binary)
+            FileSystem.Seek(1, 5)
+            FileSystem.FilePut(1, fLen)
+            FileSystem.Seek(1, 14)
+            FileSystem.FilePut(1, fill)
+            FileSystem.FileClose(1)
+
+            FileSystem.Rename(strfn, strfn & "s")
+            lstvConv.Items(fn).SubItems(1).Text = "Success"
+        Catch IOExcep As IO.IOException
+            lstvConv.Items(fn).SubItems(1).Text = IOExcep.Message
+            FileSystem.FileClose(1)
+            Exit Sub
+        End Try
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim j As Integer
+
+        For j = 0 To 498
+            fill(j) = 0
+        Next
+
+        lstvConv.Top = 5
+
+    End Sub
+
+    Private Sub cmdAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAdd.Click
+        Dim j As Integer
+
+        On Error GoTo Err
+
+        cdlOpen.Multiselect = True
+        cdlOpen.Filter = "wbf file(*.wbf)|*.wbf"
+        cdlOpen.ShowDialog()
+
+        If cdlOpen.FileName <> "" Then
+            For j = 0 To UBound(cdlOpen.FileNames)
+                lstvConv.Items.Add(cdlOpen.SafeFileNames(j))
+                lstvConv.Items(i).SubItems.Add("Waiting for converting")
+                lstvConv.Items(i).Tag = cdlOpen.FileNames(j)
+                i = i + 1
+            Next
+
+            cdlOpen.FileName = ""
+
+        End If
+Err:
+        Exit Sub
+
+    End Sub
+
+    Private Sub cmdDel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDel.Click
+        Dim j As Integer
+
+        If lstvConv.Items.Count <> 0 Then
+            Do While j <= lstvConv.Items.Count - 1
+                If lstvConv.Items(j).Selected Then
+                    lstvConv.Items.Remove(lstvConv.Items(j))
+                    i = i - 1
+                Else
+                    j = j + 1
+                End If
+            Loop
+        End If
+
+    End Sub
+
+    Private Sub cmdClr_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdClr.Click
+        lstvConv.Items.Clear()
+        i = 0
+    End Sub
+
+    Private Sub cmdConv_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdConv.Click
+        Dim j As Integer
+
+        For j = 0 To lstvConv.Items.Count - 1
+            wbf2wbfs(lstvConv.Items(j).Tag, j)
+        Next
+
+        MsgBox("All files converted", MsgBoxStyle.Information, "wbf to wbfs")
+
+    End Sub
+
+    Private Sub frmMain_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If Me.Width >= 25 Then
+            lstvConv.Width = Me.Width - 25
+        End If
+        If Me.Width >= 200 Then
+            cmdConv.Width = Me.Width - 200
+        End If
+        If Me.Height >= 75 Then
+            lstvConv.Top = 5
+            lstvConv.Height = Me.Height - 75
+        Else
+            lstvConv.Top = Me.Height - 75
+        End If
+        cmdAdd.Top = Me.Height - 64
+        cmdDel.Top = Me.Height - 64
+        cmdClr.Top = Me.Height - 64
+        cmdConv.Top = Me.Height - 64
+    End Sub
+
+End Class
